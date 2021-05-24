303a058ba947ecb334d8a495dbeb0f1efa6700ab
Yipei Zhao <zhaoyipei99@gmail.com>
2021-05-24 17:11:41 +0100

    testing on small networks

--- small_network_test.py
+++ small_network_test.py
@@ -1,1 +1,27 @@
 import networkx as nx
+from OdC import OdC
+import numpy as np
+import matplotlib.pyplot as plt
+n= 7
+m=np.linspace(1,20,19)
+graph_list = []
+for edge_number in m:
+    edge_number = int(edge_number)
+    for iteration in range(1000):
+        graph_list.append(nx.gnm_random_graph(n,edge_number))
+
+
+edge_number = []
+complexity = []
+for item in graph_list:
+    edge_number.append(len(item.edges))
+    complexity.append(OdC(item))
+
+clean_edge_number = []
+clean_complexity=[]
+for i in range(len(complexity)):
+    if not np.isnan(complexity[i]):
+        clean_edge_number.append(edge_number[i])
+        clean_complexity.append(complexity[i])
+        
+plt.scatter(clean_edge_number,clean_complexity)
