8f8bf26e30952f2d31dea2af21d8f9d3b7c2baef
Yipei Zhao <zhaoyipei99@gmail.com>
2021-06-05 16:34:06 +0100

    does not work properly

--- Ce.py
+++ Ce.py
@@ -9,13 +9,13 @@ def Ce(G,normalisation = True):
     for i in range(n):
         for j in range(i+1, n):
             E = E + 1/nx.shortest_path_length(G,nodes[i],nodes[j])
-    E = E /((n)*(n-1)/2)
+    E = 2* E /((n)*(n-1))
     if normalisation ==True:
         E_path = 0
         for i in range(1,n):
             E_path = E_path + (n)/i
         E_path = E_path *2/((n)*(n-1))
-    Ce = 4*(E-E_path)/(1-E_path)*(1- (E-E_path)/(1-E_path))
+        Ce = 4*(E-E_path)/(1-E_path)*(1- (E-E_path)/(1-E_path))
+        return Ce
     else:
         return E
-    return Ce
