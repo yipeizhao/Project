8901b1bd019154b3f97184bb1cc39d7ad1206392
Yipei Zhao <zhaoyipei99@gmail.com>
2021-06-20 12:00:17 +0100

    Update plot_result.py

--- plot_result.py
+++ plot_result.py
@@ -4,10 +4,10 @@ import Complexity
 
 #Parameter
 normalisation = True
-n = 7
-use_all_m = True
-sample = 10
-method = "Cr"
+n = 20
+use_all_m = False
+sample = 300
+method = "C1est"
 measure_method = getattr(Complexity,method)
 
 
@@ -32,32 +32,32 @@ plt.scatter(df["Number_of_edges"],result,marker = 'x',color = 'black',alpha = 0.
 #plt.scatter(small_worlds_edge,small_worlds_result,marker = 'o',color = 'red',label = "Small world")
 #plt.scatter(power_law_edge,power_law_result,color = 'blue',label = "Power law")
 
-BA_graphs = BA_random_graphs(n,sample)
-BA_edges = []
-BA_result = []
-for i in range(int(len(graphs)/5)):
-    BA_edges.append(len(BA_graphs[i].edges))
-    BA_result.append(measure_method(BA_graphs[i]))
-plt.scatter(BA_edges,BA_result,marker = 'o',color = 'red',label='BA graphs')
+# BA_graphs = BA_random_graphs(n,sample)
+# BA_edges = []
+# BA_result = []
+# for i in range(int(len(graphs)/5)):
+#     BA_edges.append(len(BA_graphs[i].edges))
+#     BA_result.append(measure_method(BA_graphs[i]))
+# plt.scatter(BA_edges,BA_result,marker = 'o',color = 'red',label='BA graphs')
 
-WS_graphs = WS_random_graphs(n,sample)
-WS_edges = []
-WS_result = []
-for i in range(int(len(graphs)/5)):
-    WS_edges.append(len(WS_graphs[i].edges))
-    WS_result.append(measure_method(WS_graphs[i]))
-plt.scatter(WS_edges,WS_result,marker = 'o',color = 'green',label='WS graphs')
-
-
-NS_graphs = NS_random_graphs(n,sample)
-NS_edges = []
-NS_result = []
-for i in range(int(len(graphs)/5)):
-    NS_edges.append(len(NS_graphs[i].edges))
-    NS_result.append(measure_method(NS_graphs[i]))
-plt.scatter(NS_edges,NS_result,marker = 'o',color = 'orange',label='NS graphs')
+# WS_graphs = WS_random_graphs(n,sample)
+# WS_edges = []
+# WS_result = []
+# for i in range(int(len(graphs)/5)):
+#     WS_edges.append(len(WS_graphs[i].edges))
+#     WS_result.append(measure_method(WS_graphs[i]))
+# plt.scatter(WS_edges,WS_result,marker = 'o',color = 'green',label='WS graphs')
 
 
+# NS_graphs = NS_random_graphs(n,sample)
+# NS_edges = []
+# NS_result = []
+# for i in range(int(len(graphs)/5)):
+#     NS_edges.append(len(NS_graphs[i].edges))
+#     NS_result.append(measure_method(NS_graphs[i]))
+# plt.scatter(NS_edges,NS_result,marker = 'o',color = 'orange',label='NS graphs')
 
-plt.title(method + "n="+str(n))
+
+
+plt.title(method+" " + "n="+str(n))
 plt.legend()
