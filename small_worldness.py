fe5ab3a9aa75a85c07c854b03bd3e32a69eb5de9
Yipei Zhao <zhaoyipei99@gmail.com>
2021-06-25 17:06:56 +0100

    Update small_worldness.py

--- small_worldness.py
+++ small_worldness.py
@@ -11,20 +11,39 @@ sample_number = 500
 method = "Ce"
 measure_method = getattr(cx,method)
 
+#   Generating random graphs
 graphs, df = ut.random_networks(n, use_all_m, sample_number )
-Cr = [0]*len(graphs)
-Lr = [0]*len(graphs)
+Cr_ratio = []
+Lr_ratio = []
+complexity = []
+small_world_c_ratio = []
+small_world_l_ratio = []
+small_world_complexity = []
+
+#   Calculating parameters for the graphs
 for i in range(len(graphs)):
     m = df["Number_of_edges"][i]
-    Cr[i] = m / (n*(n-1)/2)
-    Lr[i] = log(n)/log(df["Average_degree"][i])
-    
-Cr_ratio = [c/cr for c,cr in zip(df["Average_clustering"],Cr)]
-Lr_ratio = [l/lr for l,lr in zip(df["Average_distance"],Lr)]
+    Cr = m / (n*(n-1)/2)
+    Lr = log(n)/log(df["Average_degree"][i])
+    c = df["Average_clustering"][i]
+    l = df["Average_distance"][i]
+    if (c*Lr)/(l*Cr) < 1:
+        Cr_ratio.append(c/Cr)
+        Lr_ratio.append(l/Lr)
+        complexity.append(measure_method(graphs[i]))
+    else:
+        small_world_c_ratio.append(c/Cr)
+        small_world_l_ratio.append(l/Lr)
+        small_world_complexity.append(measure_method(graphs[i]))
 
-complexity = [measure_method(graph) for graph in graphs]
+#   Plotting the data
 plt.figure()
-plt.scatter(Cr_ratio,Lr_ratio,c = complexity)
+plt.scatter(small_world_c_ratio,small_world_l_ratio,
+            label = "Small world", marker = "x",
+            c = small_world_complexity)
+plt.scatter(Cr_ratio,Lr_ratio,c = complexity,label = "Random graphs", alpha = 0.7)
+
 plt.xlabel("C/Cr");plt.ylabel("L/Lr")
 plt.title(method + " " + "n = " + str(n))
+plt.legend()
 plt.colorbar()
