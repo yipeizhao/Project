6c765932bd2cbf1ceb2e57bae0bac728302d7949
Yipei Zhao <zhaoyipei99@gmail.com>
2021-09-11 16:35:10 +0100

    1

--- test.py
+++ test.py
@@ -4,30 +4,37 @@ import utilities as ut
 from math import log
 import numpy as np
 import matplotlib.pyplot as plt
-n = 7
-m= 11
-graphs = []
-for i in range(500):
-    g = nx.gnm_random_graph(n,m)
-    if nx.is_connected(g):
-        graphs.append(g)
+n=7
+graphs,df = ut.random_networks(n,True,50)
+# R_p = ut.redundancy(nx.path_graph(n))
+# I_p = ut.mutual_info(nx.path_graph(n))
+# R_c = ut.redundancy(nx.gnm_random_graph(n,n*(n-1)/2))
+# I_c = ut.mutual_info(nx.gnm_random_graph(n,n*(n-1)/2))
+
+
+def mari(G):
+    n = len(G.nodes)
+    R = ut.redundancy(G)
+    I = ut.mutual_info(G)
+    R_p = 2*log(2)*(n-2)/(n-1)
+    R_c = 2*log(n-1)
+    I_P = log(n-1)-log(2)*(n-3)/(n-1)
+    I_c = log(n/(n-1))
+    m=len(G.edges)
+    numerator_1 = (R-R_p)
+    numerator_2 = (I-I_c)
+    denominator = 0.25*(log(2*m)-I_c-R_p)**2
+    return numerator_1*numerator_2/denominator
+
+
+num1 = [];num2=[];result=[]
+for i in range(len(graphs)):
+    c= mari(graphs[i])
+    result.append(c)
     
-logdidj = [m*ut.redundancy(item) for item in graphs]
-min_logdidj = min(logdidj);max_logdidj = max(logdidj)
-sort_logdidj = sorted(logdidj)
-
-R_p = ut.redundancy(nx.path_graph(n))
-R_c = ut.redundancy(nx.gnm_random_graph(n,n*(n-1)*0.5))
-I_p = ut.mutual_info(nx.path_graph(n))
-I_c = ut.mutual_info(nx.gnm_random_graph(n,n*(n-1)*0.5))
-
-R_space = np.linspace(min_logdidj/m,max_logdidj/m,500)
-I_space = [log(2*m)-item for item in R_space]
-mari = [((item1-R_p)/(R_c - R_p))*((item2-I_c)/(I_p-I_c)) for item1,item2 in zip(R_space,I_space)]
-actual = [cx.MAri(item) for item in graphs]
-
-if 4*max(mari)>max(actual):
-    print(True)
-
-normalise = [item/(4*max(mari)) for item in actual]
-plt.scatter(logdidj,actual)
+plt.scatter(df["Number_of_edges"],result,label = "mari");plt.ylim([0,1])
+plt.scatter(df["Number_of_edges"],[cx.MAg(g) for g in graphs],color = "red",label = "mag");plt.ylim([0,1])
+plt.legend()
+plt.xlabel("no of edges")
+plt.ylabel("complexity")
+plt.title("n="+str(n))
